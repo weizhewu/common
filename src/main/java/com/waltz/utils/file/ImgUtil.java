0d5863c45c8f6af9066cc96a409975ce1961446b
位哲武 <weizhewu232623@gmail.com>
2023-05-22 17:03:03 +0800

    图片工具类

--- src/main/java/com/waltz/utils/file/ImgUtil.java
+++ src/main/java/com/waltz/utils/file/ImgUtil.java
@@ -2,12 +2,19 @@ package com.waltz.utils.file;
 
 import com.waltz.constant.CommonConstant;
 import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Position;
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.util.CollectionUtils;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -82,6 +89,131 @@ public class ImgUtil {
         try {
             Thumbnails.of(file)
                     .forceSize(width,height)
+                    .toOutputStream(Files.newOutputStream(Paths.get(targetPath)));
+        } catch (IOException e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 向右旋转
+     * @param file 图片
+     * @param angle 旋转度数
+     * @param targetPath 目标地址
+     * @return true：旋转成功 false：旋转失败
+     */
+    public static boolean rotateToRight(File file,Double angle,String targetPath){
+        if (!ifImg(file)){
+            return false;
+        }
+        targetPath = completeFilePath(file,targetPath);
+        try {
+            Thumbnails.of(file)
+                    .rotate(angle)
+                    .toOutputStream(Files.newOutputStream(Paths.get(targetPath)));
+        } catch (IOException e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 添加水印
+     * @param file 图片
+     * @param waterMarkFile 水印图片
+     * @param position 位置 Positions.位置
+     * @param transparency 透明度
+     * @param targetPath 目标地址
+     * @return true：添加成功 false：添加失败
+     */
+    public static boolean addWaterMark(File file, File waterMarkFile, Position position, Float transparency,String targetPath){
+        if (!ifImg(file)){
+            return false;
+        }
+        targetPath = completeFilePath(file,targetPath);
+        try {
+            Thumbnails.of(file)
+                    .watermark(position, ImageIO.read(waterMarkFile),transparency)
+                    .toOutputStream(Files.newOutputStream(Paths.get(targetPath)));
+        } catch (IOException e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 图片裁剪
+     * @param file 图片
+     * @param position 位置 Positions.位置
+     * @param width 裁剪的宽度
+     * @param height 裁剪的长度
+     * @param keepRatio 是否保持原有图片的长宽比
+     * @param targetPath 目标地址
+     * @return true：裁剪成功 false：裁剪失败
+     */
+    public static boolean curRegion(File file,Position position,Integer width,Integer height,boolean keepRatio,String targetPath){
+        if (!ifImg(file)){
+            return false;
+        }
+        targetPath = completeFilePath(file,targetPath);
+        try {
+            Thumbnails.of(file)
+                    .sourceRegion(position,width,height)
+                    .size(width,height)
+                    .keepAspectRatio(keepRatio)
+                    .toOutputStream(Files.newOutputStream(Paths.get(targetPath)));
+        } catch (IOException e){
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 图片拼接
+     * @param files 要拼接的图片
+     * @param ifHorizontal 是否水平拼接
+     * @param targetPath 目标地址
+     * @return true：拼接成功 false：拼接失败
+     */
+    public static boolean joinImages(List<File> files,boolean ifHorizontal,String targetPath){
+        if (CollectionUtils.isEmpty(files)){
+            return false;
+        }
+        for (File file : files) {
+            if (!ifImg(file)){
+                return false;
+            }
+        }
+        targetPath = completeFilePath(files.get(0),targetPath);
+        try {
+            List<BufferedImage> imageList = new ArrayList<>();
+            for (File file : files) {
+                BufferedImage image = ImageIO.read(file);
+                imageList.add(image);
+            }
+            int height = imageList.get(0).getHeight();
+            int width = imageList.get(0).getWidth();
+            if (ifHorizontal){
+                width = imageList.stream().mapToInt(BufferedImage::getWidth).sum();
+            } else {
+                height = imageList.stream().mapToInt(BufferedImage::getHeight).sum();
+            }
+            // 创建拼接后的图片画布，参数分别为宽、高、类型，这里我们使用RGB3原色类型
+            BufferedImage resultImage = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
+            Graphics graphics = resultImage.getGraphics();
+            int previousWidth = 0;
+            int previousHeight = 0;
+            for (BufferedImage image : imageList){
+                // 向画布上画图片
+                graphics.drawImage(image,previousWidth,previousHeight,null);
+                if (ifHorizontal){
+                    previousWidth += image.getWidth();
+                } else {
+                    previousHeight += image.getHeight();
+                }
+            }
+            Thumbnails.of(resultImage)
                     .toOutputStream(Files.newOutputStream(Paths.get(targetPath)));
         } catch (IOException e){
             return false;
